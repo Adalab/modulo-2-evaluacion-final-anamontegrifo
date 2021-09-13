7b19562eeb8621e1e17be3171febc8b4f293f020
Ana Montegrifo <amontegrifo@gmail.com>
2021-09-13 10:43:00 +0200

    fix reset button

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -252,9 +252,10 @@ function getLS() {
 
 //Reseteamos la sección de favoritos vaciando la sección, vaciando el array y ejecutando la función principal que añade listado y clases de las series.
 function reset() {
-	favSection.innerHTML = '';
 	favourites = [];
+	favSection.innerHTML = '';
 	addCards();
+	setLS();
 }
 
 
